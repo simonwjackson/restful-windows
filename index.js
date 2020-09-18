12e4e839e22f8517dd6009a82b1491fb71d0935c
Simon W. Jackson <simon@simonwjackson.com>
2020-09-18 15:44:40 -0500

    feat: add execShell

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param cmd {string}
+ * @return {Promise<string>}
+ */
+function execShellCommand(cmd) {
+  const { exec } = require('child_process')
+
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error)
+      }
+      resolve(stdout? stdout : stderr)
+    })
+  })
+}
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/resolution/:x/:y', (req, res) => {
+  const { x, y } = req.params
+  res.json({ x, y })
+}) 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
